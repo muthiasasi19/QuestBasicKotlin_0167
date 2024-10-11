6a06e64aae293baf155b2a88b581405d7a11a00b
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-10-11 23:57:52 +0700

    Membuat dataClass untuk menyimpan data

--- app/src/main/java/com/example/questbasickotlin_0167/DataClass.kt
+++ app/src/main/java/com/example/questbasickotlin_0167/DataClass.kt
@@ -1,2 +1,33 @@
 package com.example.questbasickotlin_0167
 
+import android.adservices.adid.AdId
+import android.provider.ContactsContract.CommonDataKinds.Email
+
+/*
+Data Class adalah class yang digunakan untuk menyimpan data.
+menyediakan fungsi untuk meng-override fungsi equals(), hashCode(), dan toString().
+Data class juga menyediakan fungsi copy() untuk membuat salinan objek dengan beberapa perubahan
+*/
+
+data class DataClass(
+    val id: Int,
+     var email: String
+)
+
+fun main() {
+    val data = DataClass(1, "indra@gmail.com")
+    println(data)
+
+    //fungsi equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data == data2) // false
+
+    //fungsi Copy
+    val data3 = data.copy()
+    println(data3)
+
+    //fungsi copy dengan perubahan
+    val data4 = data.copy(email = "sahar@gmail.com")
+    println(data4)
+
+}
