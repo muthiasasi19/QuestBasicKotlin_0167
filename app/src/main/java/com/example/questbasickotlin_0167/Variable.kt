179bee82218aa7c915cc138cb1ed03bc96e2a87b
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-10-11 22:21:38 +0700

    Mendeklarasikan variable byteValue dengan tipe data byte

--- app/src/main/java/com/example/questbasickotlin_0167/Variable.kt
+++ app/src/main/java/com/example/questbasickotlin_0167/Variable.kt
@@ -1,2 +1,13 @@
 package com.example.questbasickotlin_0167
 
+fun main() {
+
+    // Byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue") // kalau dalam string harus menggunakan '$'
+
+
+
+
+
+}
