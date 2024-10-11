ce4495994f9f69a7776edfe9b1372b967f6aec5a
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-10-11 23:33:02 +0700

    Membuat beberapa variable read only dan muteable dalam fungsi contohSet

--- app/src/main/java/com/example/questbasickotlin_0167/Collections.kt
+++ app/src/main/java/com/example/questbasickotlin_0167/Collections.kt
@@ -34,3 +34,35 @@ fun contohList() {
     val shapesLocked: List<String> = shape
     println(shapesLocked)
 }
+
+/*
+Set adalah kumpulan data yang tidak memiliki posisi atau urutan tertentu dan hanya memnyimpan data unik (tidak ada duplikasi)
+Set bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+set yang bersifat  mutebale dan readonly, artinya data yang dimasukkan dapat diubah dan tidak dapt diubah
+set read only menggunalan setOf dan set muteable menggunakan setmuteableOf
+*/
+
+fun ContohSet() {
+    println()
+    println("=== Set ===")
+
+    // Set Read-Onlu
+    val readOnlyAbjad = setOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    //List Mutable
+    val shape: MutableSet<String> = mutableSetOf("Circle", " Square", "Triangle")
+    println(shape)
+
+    // Menambahkan data ke dalam set mutable
+    shape.add("Rectangle")
+    println(shape)
+
+    //Menghapus data dari Set Mutable
+    shape.remove("Circle")
+    println(shape)
+      //set Read-Only
+    val shapesLocked: Set<String> =shape
+    println(shapesLocked)
+
+}
