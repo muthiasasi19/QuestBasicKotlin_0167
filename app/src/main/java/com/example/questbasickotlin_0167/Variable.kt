f2d0ce973f3a88a59383d15b3e987a097fed1e33
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-10-11 22:37:19 +0700

    Mendeklarasikan variable char, boolean, dan string

--- app/src/main/java/com/example/questbasickotlin_0167/Variable.kt
+++ app/src/main/java/com/example/questbasickotlin_0167/Variable.kt
@@ -18,7 +18,20 @@ fun main() {
     val doubleValue: Double = 10.5
     println("Double: $doubleValue")
 
+    // Char
+    val charValue: Char = 'A'
+    println("Char: $charValue")
 
+    //Boolean
+    val booleanValue: Boolean = true
+    println("Boolean : $booleanValue")
+
+    //String
+    var stringValue: String = "Hello, World!"
+    println("String: $stringValue")
+
+    stringValue = "Hello, Kotlin!"
+    println("String:stringValue")
 
 
 }
