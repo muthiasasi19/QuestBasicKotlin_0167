07b0d54c58b21c11997614a019f33991cbd47caf
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-10-11 22:49:19 +0700

    membuat fungsi main yang dapat menjalankan program

--- app/src/main/java/com/example/questbasickotlin_0167/Class.kt
+++ app/src/main/java/com/example/questbasickotlin_0167/Class.kt
@@ -3,3 +3,15 @@ package com.example.questbasickotlin_0167
 class Motor()
 class Contact(val id: Int, var email: String)
 
+fun main() {
+    val contact = Contact(1, "mary@gmail.com")
+
+    // Prints the value of the property: email
+    println(contact.email)
+
+    // Updates the value of the property: email
+    contact.email = "jane@gmail.com"
+
+    // Prints the new value of the property: email
+    println(contact.email)
+}
