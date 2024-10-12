26f89d85d933b70a7e885750e9c228c9e0e8afdb
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-10-12 22:53:43 +0700

    Membuat fungsi yang tidak memiliki nama

--- app/src/main/java/com/example/questbasickotlin_0167/LambdaFuntions.kt
+++ app/src/main/java/com/example/questbasickotlin_0167/LambdaFuntions.kt
@@ -1,2 +1,15 @@
 package com.example.questbasickotlin_0167
 
+// Lambda funtions adalah fungsi yang tidak memiliki nama. Biasanya digunakan untuk membuat fungsi yang sederhana.
+// Menggunakan tanda panah  (->) untuk memisahkan parameter dan body fungsi
+
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
+
+// Dapat ditulis dalam ekspresi lambda sbb:
+fun main() {
+    uppercaseString("Hello")
+    println({ string: String -> string.uppercase()}("hello"))
+    //Hello
+}
