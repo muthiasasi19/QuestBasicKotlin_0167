8b330d0337691b68a7c78ccbd7f9e153685c65e5
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-10-12 23:26:49 +0700

    perbaikan Function

--- app/src/main/java/com/example/questbasickotlin_0167/Functions.kt
+++ app/src/main/java/com/example/questbasickotlin_0167/Functions.kt
@@ -1,2 +1,38 @@
 package com.example.questbasickotlin_0167
 
+fun withoutParameter() {
+    println("== withoutParameter ==")
+    println("Hello, World")
+}
+
+fun withParameter(name:String){
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
+
+//named argument merupakan cara untuk memanggil fungsi dengan menyebutkan parameter
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("==  withNamedArgument ==")
+    println("Hello, $name! Youa are $age years old.")
+}
+
+//default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+
+fun withDefaultParameter(name: String = "Malaika", age: Int) {
+    println()
+    println("==  withDefaultParameter ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+fun withReturn(panjang : Int, lebar : Int)  {
+    return println(panjang * lebar)
+}
+
+fun main() {
+    withoutParameter()
+    withParameter("John")
+    withNamedArgument("John", age = 30)
+    withDefaultParameter(age = 25)
+}
