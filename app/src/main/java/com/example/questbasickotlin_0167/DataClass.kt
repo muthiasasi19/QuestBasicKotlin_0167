3e60c1d85be4ce111a2f8dac98d5ff6cd599a5d1
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2024-10-13 19:58:09 +0700

    perbaikan file Data Class

--- app/src/main/java/com/example/questbasickotlin_0167/DataClass.kt
+++ app/src/main/java/com/example/questbasickotlin_0167/DataClass.kt
@@ -15,19 +15,7 @@ data class DataClass(
 )
 
 fun main() {
-    val data = DataClass(1, "indra@gmail.com")
+    val data = DataClass(1, "muthia@gmail.com")
     println(data)
-
-    //fungsi equals
-    val data2 = DataClass(1, "makan@gmail.com")
-    println(data == data2) // false
-
-    //fungsi Copy
-    val data3 = data.copy()
-    println(data3)
-
-    //fungsi copy dengan perubahan
-    val data4 = data.copy(email = "sahar@gmail.com")
-    println(data4)
 
 }
